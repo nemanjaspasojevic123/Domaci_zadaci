97f59f9f7d18e4e84436278f50f9322caa0d35cc
Nemanja Spasojevic <nemanja.spasojevic.memi@gmail.com>
2020-02-03 19:26:08 +0100

    dodat zadatak 1

---
+++ Nedelja2/zadatak1.js
@@ -0,0 +1,8 @@
+let a = 5;
+let b = 2;
+console.log ('Zbir=', a + b)
+console.log('Razlika=', a - b)
+console.log('Proizvod=', a * b)
+console.log('Kolicnik=', a / b)
+console.log('Ostatak pri deljenju=', a % b)
+console.log('Stepen=', a ** b)
