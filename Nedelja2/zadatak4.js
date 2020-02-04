086b30e8fb03216cc43659beca48825d24d5c3f5
Nemanja Spasojevic <nemanja.spasojevic.memi@gmail.com>
2020-02-04 21:53:50 +0100

    dodat zadatak 3 i zadatak 4

---
+++ Nedelja2/zadatak4.js
@@ -0,0 +1,15 @@
+let n = 1;
+let m = 5005;
+for(var i = n; i < m; i++)
+if (i % 5 == 0){
+    console.log(i)
+}
+
+//Drugi nacin
+//
+// let x = 5;
+// while(x < 5005){
+//     if(x % 5 == 0)
+//     console.log(x);
+//     x++;
+// }
