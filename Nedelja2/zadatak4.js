347d7ad9817b1edffa7762459f288e32bbc8ead1
Nemanja Spasojevic <nemanja.spasojevic.memi@gmail.com>
2020-02-04 23:56:56 +0100

    dodatak 4

--- Nedelja2/zadatak4.js
+++ Nedelja2/zadatak4.js
@@ -1,11 +1,23 @@
-let n = 1;
-let m = 5005;
-for(var i = n; i < m; i++)
-if (i % 5 == 0){
-    console.log(i)
+let brojac = 0;
+for( let x = 5; brojac < 1000; x++){
+    if(x % 5 == 0){
+        console.log(x);
+        brojac++;
+    }
 }
 
+
 //Drugi nacin
+//
+// let n = 1;
+// let m = 5005;
+// for(var i = n; i < m; i++)
+// if (i % 5 == 0){
+//     console.log(i)
+// }
+
+
+//Treci nacin
 //
 // let x = 5;
 // while(x < 5005){
